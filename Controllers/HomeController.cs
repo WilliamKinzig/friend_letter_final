b7081596939d4cee11fc0c445248a6e8dd136889
WilliamKinzig <wippawill2@gmail.com>
2018-07-12 22:46:09 -0700

    create first VIEW called Letter, now /hello /goodbye and / are all working.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,23 @@ namespace FriendLetter.Controllers
 {
     public class HomeController : Controller
     {
-        public string Hello() { return "Hello frienddddddddddddddd!"; }
+        // //http://localhost:5000/home/hello
+        // public string Hello() { return "Hello frienddddddddddddddd!"; }
+        // //http://localhost:5000/home/goodbye
+        // public string Goodbye() { return "Goodbye friendddddddddddddd."; }
+
+        //http://localhost:5000/hello
+        [Route("/hello")]
+        public string Hello() { return "Hello friend!"; }
+        //http://localhost:5000/goodbye
+        [Route("/goodbye")]
+        public string Goodbye() { return "Goodbye friend."; }
+
+        // [Produces("text/html")]
+        [Route("/")]
+        public ActionResult Letter()
+        {
+          return View();
+        }
     }
 }
