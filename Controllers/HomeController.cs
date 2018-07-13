2a908905f2d8c9610706832a3f7c491d633be1c1
WilliamKinzig <wippawill2@gmail.com>
2018-07-12 23:05:48 -0700

    create second VIEW called Journal. localhost:5000/hello /goodbye /journal /letter, all working.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FriendLetter.Controllers
 {
+    //Tells view() to look in home folder
     public class HomeController : Controller
     {
         // //http://localhost:5000/home/hello
@@ -17,8 +18,19 @@ namespace FriendLetter.Controllers
         public string Goodbye() { return "Goodbye friend."; }
 
         // [Produces("text/html")]
-        [Route("/")]
+
+        //http://localhost:5000/letter
+        [Route("/letter")]
         public ActionResult Letter()
+        {
+          // Looks for a template with the same name as its parent method
+          // return that template as an object of the type ActionResult
+          return View();
+        }
+
+        //http://localhost:5000/journal
+        [Route("/journal")]
+        public ActionResult Journal()
         {
           return View();
         }
